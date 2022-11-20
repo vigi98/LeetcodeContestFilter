c726f8e796ca9958f9edf8e865456f62e7ce91d7
Vignesh Hegde <vigneshhegde98@gmail.com>
2022-11-20 18:45:02 +0530

    Create langExtns.js

---
+++ util/langExtns.js
@@ -0,0 +1,18 @@
+const extnMap={};
+extnMap.java='.java';
+extnMap.python='.py';
+extnMap.cpp='.cpp';
+extnMap.c='.c';
+extnMap.javascript='.js';
+extnMap.csharp='.cs';
+extnMap.ruby='.rb';
+extnMap.go='.go';
+extnMap.php='.php';
+extnMap.kotlin='.kt';
+extnMap.typescript='.ts';
+extnMap.swift='.swift';
+extnMap.scala='.sc';
+extnMap.dart='.dart';
+extnMap.rust='.rs';
+
+module.exports=extnMap;
